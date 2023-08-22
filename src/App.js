6a237cb70697065d009b1cb8b5ac2119af236e4b
Brandon Ojeda <brandono9192@gmail.com>
2023-08-22 03:16:05 -0400

    Set up router-dom & react-helmet for homepage.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from './components/homepage';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/homepage.js
@@ -0,0 +1,21 @@
+import React , {Fragment} from 'react';
+import { Helmet } from 'react-helmet';
+
+const Home = () =>  (
+
+    <Fragment> 
+        <Helmet><title>Quiz App - Home</title></Helmet>
+        <div id="home">
+            <section>
+                
+            </section>
+            <h1>Home Page</h1>
+        </div>
+    </Fragment>
+        
+     
+ 
+    )
+
+
+export default Home;
