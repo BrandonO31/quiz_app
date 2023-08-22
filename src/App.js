fae211ed5cafb6175d6da1df1e8c7211eb667de9
Brandon Ojeda <brandono9192@gmail.com>
2023-08-22 13:35:02 -0400

    added content to home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import '@mdi/font/css/materialdesignicons.min.css';
 import Home from './components/homepage';
 
 function App() {
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,5 +1,8 @@
 import React , {Fragment} from 'react';
 import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
+
+
 
 const Home = () =>  (
 
@@ -7,9 +10,25 @@ const Home = () =>  (
         <Helmet><title>Quiz App - Home</title></Helmet>
         <div id="home">
             <section>
+                <div>
+                    <span className="mdi mdi-book-open-variant mdi-48px"></span>
+                </div>
+                <h1>Quiz App</h1>
+                <div className="play-button-container">
+                    <ul>
+                        <li><Link to="/play/instructions">Play</Link></li>
+                    </ul>
+                </div>
+                <div className="desc-container">
+                    <p>This is where the instructions will be written</p>
+                </div>
+                
+                    
+      
+
                 
             </section>
-            <h1>Home Page</h1>
+            
         </div>
     </Fragment>
         
